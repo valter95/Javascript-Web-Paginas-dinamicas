beec79ff4d33a607ad98423ec86d21fdbc5dc823
Valter <valterhenrique39@gmail.com>
2022-08-03 17:54:53 -0300

    Criado functions para trabalhar com os eventos de click e teclas para emitir o som, também foi aprendido os tipos de operadores e condições do javascript.

--- main.js
+++ main.js
@@ -1,1 +1,38 @@
-document.querySelector('.tecla_pom')
+function TocaSom(IdElementoAudio)
+{
+    const elemento = document.querySelector(IdElementoAudio);
+
+    if(elemento != null && elemento.localName === 'audio')
+    {
+        elemento.play;
+    }
+    else
+    {
+        alert("Elemento não encontrado");
+    }
+}
+
+const listaDeTeclas = document.querySelectorAll('.tecla');
+
+for(let contador = 0; contador <listaDeTeclas.length; contador ++)
+{
+    const tecla =  listaDeTeclas[contador];
+
+    const instrumento = tecla.classList[1];
+    tecla.onclick = function () {
+        TocaSom(`#som_${instrumento}`);
+    };
+
+    tecla.onkeydown =  function(evento)
+    {
+        if(evento.code === 'Enter' || evento.code === 'Space'){
+        tecla.classList.add('ativa');
+        };
+    }
+    tecla.onkeyup =  function(evento)
+    {
+       
+        tecla.classList.remove('ativa');
+        
+    }
+}
